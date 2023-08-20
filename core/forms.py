efaf5ebfaa1c32f433670632c3c9c278c749a2c5
edward <gilfoyle.arthur@protonmail.com>
2023-08-20 08:15:18 +0000

    corrected invalid date error

--- core/forms.py
+++ core/forms.py
@@ -5,3 +5,7 @@ class AttendanceForm(forms.ModelForm):
     class Meta:
         model = Attendance
         fields = ['first_name', 'last_name', 'email', 'phone_number', 'mode_of_payment', 'attendance', 'date_of_payment']
+    
+    date_of_payment = forms.DateField(
+        widget=forms.DateInput(attrs={'type': 'date'}),
+    )
