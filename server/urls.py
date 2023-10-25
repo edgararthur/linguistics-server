c1e6445c4c73aa3e090885ca85711693ec117490
edgararthur <gilfoyle.arthur@protonmail.com>
2023-10-25 12:07:21 +0000

    updated static files url

--- server/urls.py
+++ server/urls.py
@@ -17,3 +17,5 @@ urlpatterns = [
 urlpatterns += staticfiles_urlpatterns()
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
