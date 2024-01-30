8839bdb5f4c017e6308e0e16e022ce51a5eb86c2
edgararthur <gilfoyle.arthur@protonmail.com>
2024-01-30 00:42:55 +0000

    changed request of all leadership model

--- core/views.py
+++ core/views.py
@@ -7,8 +7,8 @@ def attend_conference(request):
     if request.method == 'POST':
         form = AttendanceForm(request.POST)
         if form.is_valid():
-            form.save()  # Save the form data to the database
-            return HttpResponse("Success")  # Redirect to a success page
+            form.save() 
+            return HttpResponse("Success")
     else:
         form = AttendanceForm()
     
